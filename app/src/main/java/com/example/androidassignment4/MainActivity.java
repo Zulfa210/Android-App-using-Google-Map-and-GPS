5c7b62e70f509ae9130de87c7826d46ea20956db
Zulfa210 <zulfaattar@gmail.com>
2021-01-17 13:05:11 +0530

    Added the code

---
+++ app/src/main/java/com/example/androidassignment4/MainActivity.java
@@ -0,0 +1,296 @@
+package com.example.androidassignment4;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    FusedLocationProviderClient
+            mFusedLocationClient;
+
+    // Initializing other items
+    // from layout file
+    TextView latTextView, lonTextView;
+    int PERMISSION_ID = 44;
+    Button mapshow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        latTextView = findViewById(R.id.latTextView);
+        lonTextView = findViewById(R.id.lonTextView);
+
+        mapshow = (Button) findViewById(R.id.button);
+
+
+        mFusedLocationClient
+                = LocationServices
+                .getFusedLocationProviderClient(this);
+
+        // method to get the location
+        getLastLocation();
+    }
+
+    private void getLastLocation()
+    {
+        // check if permissions are given
+        if (checkPermissions()) {
+
+            // check if location is enabled
+            if (isLocationEnabled()) {
+
+                // getting last
+                // location from
+                // FusedLocationClient
+                // object
+                mFusedLocationClient
+                        .getLastLocation()
+                        .addOnCompleteListener(
+                                new OnCompleteListener<Location>() {
+
+                                    @Override
+                                    public void onComplete(
+                                            @NonNull Task<Location> task)
+                                    {
+                                        Location location = task.getResult();
+                                        if (location == null) {
+                                            requestNewLocationData();
+                                        }
+                                        else {
+                                            latTextView
+                                                    .setText(
+                                                            location
+                                                                    .getLatitude()
+                                                                    + "");
+                                            lonTextView
+                                                    .setText(
+                                                            location
+                                                                    .getLongitude()
+                                                                    + "");
+
+                                            mapshow.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View v) {
+                                                    Intent int1 = new Intent(MainActivity.this, MapsActivity.class);
+                                                    int1.putExtra("Latitude", location.getLatitude());
+                                                    int1.putExtra("Longitude", location.getLongitude());
+                                                    startActivity(int1);
+
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+            }
+
+            else {
+                Toast
+                        .makeText(
+                                this,
+                                "Please turn on"
+                                        + " your location...",
+                                Toast.LENGTH_LONG)
+                        .show();
+
+                Intent intent
+                        = new Intent(
+                        Settings
+                                .ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        }
+        else {
+            // if permissions aren't available,
+            // request for permissions
+            requestPermissions();
+        }
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData()
+    {
+
+        // Initializing LocationRequest
+        // object with appropriate methods
+        LocationRequest mLocationRequest
+                = new LocationRequest();
+        mLocationRequest.setPriority(
+                LocationRequest
+                        .PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        // setting LocationRequest
+        // on FusedLocationClient
+        mFusedLocationClient
+                = LocationServices
+                .getFusedLocationProviderClient(this);
+
+        mFusedLocationClient
+                .requestLocationUpdates(
+                        mLocationRequest,
+                        mLocationCallback,
+                        Looper.myLooper());
+    }
+
+    private LocationCallback
+            mLocationCallback
+            = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(
+                LocationResult locationResult)
+        {
+            Location mLastLocation
+                    = locationResult
+                    .getLastLocation();
+            latTextView
+                    .setText(
+                            "Latitude: "
+                                    + mLastLocation
+                                    .getLatitude()
+                                    + "");
+            lonTextView
+                    .setText(
+                            "Longitude: "
+                                    + mLastLocation
+                                    .getLongitude()
+                                    + "");
+
+
+            mapshow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent int1 = new Intent(MainActivity.this, MapsActivity.class);
+                    int1.putExtra("Latitude", mLastLocation.getLatitude());
+                    int1.putExtra("Longitude", mLastLocation.getLongitude());
+                    startActivity(int1);
+
+                }
+            });
+        }
+    };
+
+    // method to check for permissions
+    private boolean checkPermissions()
+    {
+        return ActivityCompat
+                .checkSelfPermission(
+                        this,
+                        Manifest.permission
+                                .ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+
+                && ActivityCompat
+                .checkSelfPermission(
+                        this,
+                        Manifest.permission
+                                .ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
+
+        // If we want background location
+        // on Android 10.0 and higher,
+        // use:
+        /* ActivityCompat
+                .checkSelfPermission(
+                    this,
+                    Manifest.permission
+                        .ACCESS_BACKGROUND_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        */
+    }
+
+    // method to requestfor permissions
+    private void requestPermissions()
+    {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] {
+                        Manifest.permission
+                                .ACCESS_COARSE_LOCATION,
+                        Manifest.permission
+                                .ACCESS_FINE_LOCATION },
+                PERMISSION_ID);
+    }
+
+    // method to check
+    // if location is enabled
+    private boolean isLocationEnabled()
+    {
+        LocationManager
+                locationManager
+                = (LocationManager)getSystemService(
+                Context.LOCATION_SERVICE);
+
+        return locationManager
+                .isProviderEnabled(
+                        LocationManager.GPS_PROVIDER)
+                || locationManager
+                .isProviderEnabled(
+                        LocationManager.NETWORK_PROVIDER);
+    }
+
+    // If everything is alright then
+    @Override
+    public void
+    onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults)
+    {
+        super
+                .onRequestPermissionsResult(
+                        requestCode,
+                        permissions,
+                        grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0
+                    && grantResults[0]
+                    == PackageManager
+                    .PERMISSION_GRANTED) {
+
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+    }
+}
